6c439bf23b6dc5dc38c3ea8e058c4e3aeb05b99f
moly <molypap1213@gmail.com>
2026-02-17 23:32:51 +0300

    In working

--- config/config.py
+++ config/config.py
@@ -12,6 +12,22 @@ class Logging:
     format: str
 
 @dataclass
+class Postgres:
+    host: str
+    port: int
+    user: str
+    password: str
+    database: str
+
+
+@dataclass
+class Redis:
+    host: str
+    port: int
+    database: str
+    password: str
+    username: str
+@dataclass
 class OpenRouter:
     token: str
 
@@ -20,6 +36,8 @@ class Config:
     bot:TgBot
     logging:Logging
     ai:OpenRouter
+    pg_settings:Postgres
+    redis_settings:Redis
 
 def get_config(path:str |None= None)->Config:
     env = Env()
@@ -27,5 +45,19 @@ def get_config(path:str |None= None)->Config:
     return Config(
         TgBot(token=env("BOT_TOKEN")),
         Logging(level=env("LOG_LEVEL"), format= env("LOG_FORMAT")),
-        OpenRouter(token=env("OPENROUTER_TOKEN"))
+        OpenRouter(token=env("OPENROUTER_TOKEN")),
+        Postgres(
+            host=env("PG_HOST"),
+            port=env.int("PG_PORT"),
+            database=env("PG_DB"),
+            user=env("PG_USER"),
+            password=env("PG_PASS"),
+        ),
+        Redis(
+            host=env("REDIS_HOST"),
+            port=env.int("REDIS_PORT"),
+            database=env("REDIS_DATABASE"),
+            password=env("REDIS_PASSWORD"),
+            username=env("REDIS_USERNAME"),
+        )
     )
