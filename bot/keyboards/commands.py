5c73877a9ab24bf78c53c1edb438094fa7896479
moly <molypap1213@gmail.com>
2026-05-01 20:38:53 +0300

    expose port

--- bot/keyboards/commands.py
+++ bot/keyboards/commands.py
@@ -4,8 +4,8 @@ from aiogram import Bot
 async def get_commands(bot: Bot):
     try:
         commands = [
-            BotCommand(command='/start', description='Запустить/перезагрузить бота'),
-            BotCommand(command='/getInfo', description='Информация о боте')
+            BotCommand(command='/start', description='reload bot'),
+            BotCommand(command='/info', description='bot info')
         ]
         await bot.set_my_commands(commands)
         print("Команды успешно установлены.")
--- bot/main.py
+++ bot/main.py
@@ -2,12 +2,14 @@ from config.config import Config, get_config
 from database.connection import get_pg_connection
 from psycopg import AsyncConnection
 from aiogram import Bot, Dispatcher
-from aiogram.fsm.storage.redis import RedisStorage
 from aiogram.fsm.storage.memory import MemoryStorage
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import parse_mode
-from redis.asyncio import Redis
-from database.queries import get_id_admins_query
+from aiohttp import web
+from aiogram.webhook.aiohttp_server import SimpleRequestHandler, setup_application
+import os
+import asyncio
+import logging
 from keyboards.commands import get_commands
 from handlers.other_handler import other_router
 from handlers.load_every_day_report_handler import main_router
@@ -18,48 +20,40 @@ from handlers.sport_traker_handler import sport_traker
 from handlers.tracker_imt_handler import tracker_imt_router
 from handlers.admins_handler import admin_router
 from handlers.integrations_hanler import integrations_router
-from utils.reminders import start_reminders, send_daily_remind
-import asyncio
-import logging
+from utils.reminders import start_reminders
 
+config: Config = get_config()
 
-config:Config = get_config()
-
-logging.basicConfig(level=config.logging.level,
-                    format = config.logging.format)
+logging.basicConfig(level=config.logging.level, format=config.logging.format)
 logger = logging.getLogger(__name__)
-
-
-
 
 
 async def main():
     logger.info('Starting bot ...')
+
     storage = MemoryStorage()
-    # storage = RedisStorage(
-    #     redis = Redis(
-    #         host = config.redis_settings.host,
-    #         port = config.redis_settings.port,
-    #         db = config.redis_settings.database,
-    #         password=config.redis_settings.password,
-    #         username=config.redis_settings.username,
-    #     )
-    # )
-    bot:Bot = Bot(token = config.bot.token,
-                  default= DefaultBotProperties(parse_mode= parse_mode.ParseMode.HTML))
-    dp = Dispatcher(storage = storage)
-    conn:AsyncConnection = await get_pg_connection(
+
+    bot: Bot = Bot(
+        token=config.bot.token,
+        default=DefaultBotProperties(parse_mode=parse_mode.ParseMode.HTML)
+    )
+
+    dp = Dispatcher(storage=storage)
+
+    conn: AsyncConnection = await get_pg_connection(
         config.pg_settings.database,
         config.pg_settings.user,
         config.pg_settings.password,
         config.pg_settings.host,
         config.pg_settings.port,
     )
-    admins = await get_id_admins_query(conn)
+
+
     await get_commands(bot)
-    dp['admins'] = set(admins)
+
     dp['conn'] = conn
     dp['bot'] = bot
+
     dp.include_router(main_router)
     dp.include_router(get_reports_router)
     dp.include_router(statistics_router)
@@ -70,10 +64,28 @@ async def main():
     dp.include_router(admin_router)
     dp.include_router(other_router)
 
-    start_reminders(bot,conn)
+    start_reminders(bot, conn)
 
-    await dp.start_polling(bot, disable_notifications=True)
-    logger.info('Bot started.')
+
+    WEBHOOK_PATH = "/webhook"
+    BASE_URL = os.getenv("RENDER_EXTERNAL_URL")
+
+    await bot.delete_webhook(drop_pending_updates=True)
+    webhook_url = f"{BASE_URL.rstrip('/')}{WEBHOOK_PATH}"
+    await bot.set_webhook(webhook_url)
+
+    app = web.Application()
+    SimpleRequestHandler(dispatcher=dp, bot=bot).register(app, path=WEBHOOK_PATH)
+    setup_application(app, dp, bot=bot)
+
+    port = int(os.getenv("PORT", 10000))
+    runner = web.AppRunner(app)
+    await runner.setup()
+    site = web.TCPSite(runner, host='0.0.0.0', port=port)
+    await site.start()
+
+    logger.info(f"Bot started on port {port}")
+    await asyncio.Event().wait()
 
 
 if __name__ == '__main__':
